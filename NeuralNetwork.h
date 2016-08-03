12db1d5a621af7a30e91fffd4ed6ebebd9373952
JaeMin-Choi <jaemin.jaemni@gmail.com>
2016-08-03 11:38:28 +0900

    public method delete() moved into private method

--- NeuralNetwork.h
+++ NeuralNetwork.h
@@ -5,12 +5,12 @@ class NeuralNetwork {
 private:
 	int numberOfLayer;
 	Layer* aLayer;
-
+	void Delete();
 public:
 	NeuralNetwork();
 	NeuralNetwork(int inputDim, int numLayer, int* eachLayer_outputDim);
 	~NeuralNetwork() { Delete(); }
-	void Delete();
+	
 
 	void Init(int inputDim, int numLayer, int* eachLayer_outputDim);
 	int Is_Inited() { return aLayer != NULL; } //aLayer가 할당되어 있다면, return 1, 할당되어있지 않다면(==NULL) return 0
